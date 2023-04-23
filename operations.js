693c40e9b090495943597d71de96ac0a6c136a0b
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-04-23 23:59:19 +0500

    add cp

--- operations.js
+++ operations.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs/promises');
-const { statSync, createReadStream } = require('fs');
+const { statSync, createReadStream, createWriteStream } = require('fs');
 const { printMessage } = require('./lib/helper');
 
 const operations = ['up', 'cd', 'ls', 'cat', 'add', 'rn', 'cp', 'mv', 'rm'];
@@ -24,6 +24,9 @@ const operate = async (operation, params) => {
             break;
         case 'rn':
             await rn(params[0], params[1]);
+            break;
+        case 'cp':
+            await cp(params[0], params[1]);
             break;
         default:
             throw new Error('Operation failed');
@@ -79,6 +82,16 @@ const add = (fileName) => {
 const rn = async (filePath, newName) => {
     const newFilePath = path.join(path.dirname(filePath), newName);
     await fs.rename(filePath, newFilePath);
+};
+
+const cp = async (filePath, pathDir) => {
+    const fileName = path.basename(filePath);
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(path.join(pathDir, fileName));
+
+    readStream.on('data', (chunk) => {
+        writeStream.write(chunk);
+    });
 };
 
 module.exports = { operations, operate };
